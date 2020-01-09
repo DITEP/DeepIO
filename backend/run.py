301304b521e4dbcdffcc3a604dcf05f3b86fc790
Louis Rebaud <louis.rebaud@gustaveroussy.fr>
2020-01-09 10:12:24 +0100

    add the prediction engine that run in parallel of the app

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 from application import create_app
+from prediction_engine import start_prediction_engine
+
+# start prediction engine
+start_prediction_engine()
+
+# start the app
 app = create_app()
-app.run(host="0.0.0.0", port=8002)
+app.run(host="0.0.0.0", port=8002, use_reloader=False)
